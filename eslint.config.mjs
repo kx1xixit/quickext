96b7c83e485b8a8ff74b594b35cbc4dd9da0e3a8
Kane <kmboswell@proton.me>
2026-02-09 15:20:42 +0000

    Upgrade ESLint's config to Flat

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+
+export default [
+  {
+    ignores: ['node_modules/', 'build/']
+  },
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        Scratch: 'readonly'
+      }
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': [
+        'warn',
+        {
+          args: 'none'
+        }
+      ],
+      'no-console': 'off',
+      'no-var': 'warn',
+      'prefer-const': 'warn'
+    }
+  }
+];
